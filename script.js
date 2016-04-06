6c5ce40d9ffcd03303c6d26b4fa125ce48d7e8ab
kevinorta <kevino6532@hstat.org>
2016-04-06 17:48:42 +0000

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+$(".button-collapse").sideNav();
+
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
